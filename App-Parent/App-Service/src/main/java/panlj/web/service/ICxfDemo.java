cf7fca34ac7ebe61c34ba4bb0d25cab68398635c
林江 <@192.168.3.2>
2014-05-27 01:22:23 +0800

    1

--- App-Parent/App-Service/src/main/java/panlj/web/service/ICxfDemo.java
+++ App-Parent/App-Service/src/main/java/panlj/web/service/ICxfDemo.java
@@ -1,9 +1,12 @@
 package panlj.web.service;
 
 import javax.jws.WebService;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
 @WebService
 public interface  ICxfDemo {
+	@GET
+	@Path("/sayHello")
     public String sayHello(String foo);
 }
--- App-Parent/App-Service/src/main/java/panlj/web/service/impl/ICxfDemoImpl.java
+++ App-Parent/App-Service/src/main/java/panlj/web/service/impl/ICxfDemoImpl.java
@@ -1,11 +1,32 @@
 package panlj.web.service.impl;
 
 import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+
+import panlj.web.service.ICxfDemo;
 
 @WebService()
-public class ICxfDemoImpl {
+public class ICxfDemoImpl implements ICxfDemo {
 
-    public String sayHello(String foo) {
-        return "hello "+foo;
-    }
+	@GET
+	@Path("/sayHello")
+	public String sayHello(String foo) {
+		return "hello " + foo;
+	}
+	
+	/**
+	 * 静态调用方法
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ICxfDemo service = new ICxfDemoImpl();
+		// Service instance
+		JAXRSServerFactoryBean restServer = new JAXRSServerFactoryBean();
+		restServer.setServiceBean(service);
+		restServer.setAddress("http://localhost:8080/");
+		restServer.create();
+	}
 }

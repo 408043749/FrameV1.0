35ad44d8a8113c7b18b0036a00e2ac7c2e91609e
linjiangpan <linjiangpan@WSWINCNHZ1381.hengtiansoft.com>
2014-05-30 17:15:10 +0800

    init domains

--- App-Parent/App-Services/src/main/java/panlj/web/service/ICxfDemo.java
+++ App-Parent/App-Services/src/main/java/panlj/web/service/ICxfDemo.java
@@ -1,15 +1,45 @@
 package panlj.web.service;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
+import org.App.Domains.App;
+
+
+@Path("/iCxfDemo")
 public interface  ICxfDemo {
+	
+	/**
+	 * 单参数传递
+	 * @param foo
+	 * @return
+	 */
 	@GET
 	@Path("/sayHello/{foo}")
     public String sayHello(@PathParam("foo")String foo);
 	
+	/**
+	 * 多参数传递
+	 * @param name
+	 * @param pwd
+	 * @return
+	 */
 	@GET
-	@Path("/sayHello2")
+	@Path("/sayHello2/{name}/{pwd}")
 	public String sayHello2(@PathParam("name")String name,@PathParam("pwd")String pwd);
+	
+	/**
+	 * 传递对象
+	 * @param app
+	 * @return
+	 */
+	@POST
+	@Path("/getApp")
+	@Produces("application/json")
+    @Consumes("application/json")
+	public App getApp(App app);
 }
--- App-Parent/App-Services/src/main/java/panlj/web/service/impl/ICxfDemoImpl.java
+++ App-Parent/App-Services/src/main/java/panlj/web/service/impl/ICxfDemoImpl.java
@@ -1,16 +1,15 @@
 package panlj.web.service.impl;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.App.Domains.App;
+import org.apache.cxf.jaxrs.client.WebClient;
 import org.springframework.stereotype.Service;
 
 import panlj.web.service.ICxfDemo;
 
 @Service(value="iCxfDemoImpl")
-@Path("/iCxfDemoImpl")
 public class ICxfDemoImpl implements ICxfDemo {
 
 	public String sayHello(String foo) {
@@ -21,6 +20,19 @@ public class ICxfDemoImpl implements ICxfDemo {
 		return "result: "+name+","+pwd;
 	}
 	
+	public App getApp(App app) {
+		App newApp = new App();
+		newApp.setId("000000001");
+		newApp.setPwd(app.getPwd());
+		newApp.setName(app.getName());
+		List<String> list = new ArrayList<String>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		newApp.setChilds(list);
+		
+		return newApp;
+	}
 	/**
 	 * 静态调用方法
 	 * @param args
@@ -28,10 +40,17 @@ public class ICxfDemoImpl implements ICxfDemo {
 	public static void main(String[] args) {
 		ICxfDemo service = new ICxfDemoImpl();
 		// Service instance
-		JAXRSServerFactoryBean restServer = new JAXRSServerFactoryBean();
-		restServer.setServiceBean(service);
-		restServer.setAddress("http://localhost:8080/");
-		restServer.create();
+//		JAXRSServerFactoryBean restServer = new JAXRSServerFactoryBean();
+//		restServer.setServiceBean(service);
+//		restServer.setAddress("http://localhost:8080/");
+//		restServer.create();
+		App app = new App();
+		app.setName("PANLJ");
+		app.setPwd("123456");
+		WebClient client = WebClient.create("http://localhost:8080/App-Services/iCxfDemo");
+		client.path("/getApp/panlj");
+		App resultApp = client.post(app,App.class);
+		System.out.println(resultApp.getId());
 	}
 
 }
